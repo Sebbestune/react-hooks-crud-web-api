5445349854c5325635ecdd37c2bbca3714faea98
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2021-09-22 00:56:04 +0300

    workshop

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,2 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://localhost:8080/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
+// Here you should import axios and then
+// export default axios create
